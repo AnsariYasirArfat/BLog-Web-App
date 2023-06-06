ab7ac9151035b7c7d283feb4e206dfb8b3ed3907
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-06 22:03:09 +0530

    Adding Blog functionality added

--- client/JavaScript.js
+++ client/JavaScript.js
@@ -1,14 +1,42 @@
 const blogList = document.getElementById("blogList");
 
+// fetching data through API
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((blog) => {
-      const li = document.createElement("li");
-      li.innerHTML = `<h3>${blog.title}</h3><p>${blog.body}</p>`;
-      blogList.appendChild(li);
+      const div = document.createElement("div");
+      div.innerHTML = `<h3>${blog.title}</h3><p>${blog.body}</p>`;
+      blogList.appendChild(div);
     });
   })
   .catch((error) => {
     console.log("Error:", error);
   });
+
+// Adding blog
+
+const blogForm = document.getElementById("blogForm");
+const titleInput = document.getElementById("titleInput");
+const contentInput = document.getElementById("contentInput");
+
+blogForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  if (title && content) {
+    const newBlog = {
+      title: title,
+      body: content,
+    };
+
+    const div = document.createElement("div");
+    div.innerHTML = `<h3>${newBlog.title}</h3><p>${newBlog.body}</p>`;
+    blogList.appendChild(div);
+
+    titleInput.value = "";
+    contentInput.value = "";
+  }
+});
