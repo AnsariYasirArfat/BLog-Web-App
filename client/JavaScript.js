ae6b11a6b0727a64a73cfde694877a149f145837
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-06 22:58:34 +0530

    Deleting Blog functionality added

--- client/JavaScript.js
+++ client/JavaScript.js
@@ -1,12 +1,11 @@
 const blogList = document.getElementById("blogList");
 
-// fetching data through API
+// Fetching data through API
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((blog) => {
-      const div = document.createElement("div");
-      div.innerHTML = `<h3>${blog.title}</h3><p>${blog.body}</p>`;
+      const div = createBlogDiv(blog);
       blogList.appendChild(div);
     });
   })
@@ -14,8 +13,22 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     console.log("Error:", error);
   });
 
-// Adding blog
+// Helper function to create a blog div element
+function createBlogDiv(blog) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>${blog.title}</h3>
+    <p>${blog.body}</p>
+    <button class="delete-btn" data-id="${blog.id}">Delete</button>
+  `;
 
+  const deleteButton = div.querySelector(".delete-btn");
+  deleteButton.addEventListener("click", deleteBlog);
+
+  return div;
+}
+
+// Adding blog
 const blogForm = document.getElementById("blogForm");
 const titleInput = document.getElementById("titleInput");
 const contentInput = document.getElementById("contentInput");
@@ -32,11 +45,29 @@ blogForm.addEventListener("submit", (e) => {
       body: content,
     };
 
-    const div = document.createElement("div");
-    div.innerHTML = `<h3>${newBlog.title}</h3><p>${newBlog.body}</p>`;
+    const div = createBlogDiv(newBlog);
     blogList.appendChild(div);
-
-    titleInput.value = "";
-    contentInput.value = "";
   }
+
+  titleInput.value = "";
+  contentInput.value = "";
 });
+
+// Function to delete a blog
+function deleteBlog(event) {
+  const blogId = event.target.dataset.id;
+  const blogDiv = event.target.parentNode;
+
+  // Make a DELETE request to the API
+  fetch(`https://jsonplaceholder.typicode.com/posts/${blogId}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      // Remove the blog div from the UI
+      blogDiv.remove();
+      console.log("removed");
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
